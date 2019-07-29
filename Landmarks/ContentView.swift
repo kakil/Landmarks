6676458606f78411081f7f8599ea91d9a066e870
Kitwana Akil <kakil@itelinc.com>
2019-07-29 08:02:48 -0400

    Completed sections 1-3

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Kitwana Akil on 7/29/19.
+//  Copyright © 2019 Kitwana Akil. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+                .foregroundColor(Color.black)
+                HStack {
+                    Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
+                        .font(.subheadline)
+                        Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+        }
+        .padding()
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
