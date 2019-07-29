7f955d011f3e448adff66973c2da6c1c4155a6fd
Kitwana Akil <kakil@itelinc.com>
2019-07-29 08:49:34 -0400

    Completed Section 6.  Completed SwiftUI Essentials - Creating and Combining Views.

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,19 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-                .foregroundColor(Color.black)
-                HStack {
-                    Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
-                        .font(.subheadline)
-                        Spacer()
-                    Text("California")
-                        .font(.subheadline)
-                }
-        }
-        .padding()
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(Color.black)
+                    HStack {
+                        Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
+                            .font(.subheadline)
+                            Spacer()
+                            Text("California")
+                                .font(.subheadline)
+                        }
+                    }
+                    .padding()
+                    Spacer()
+            }
     }
 }
 
