55e450b04820185237dd3fbe5bd69e1558869e66
Kitwana Akil <kakil@itelinc.com>
2019-08-07 08:56:55 -0400

    Completed section 7.  Handling User Input complete.

--- HandlingUserInput/Complete/Landmarks/Landmarks/LandmarkDetail.swift
+++ HandlingUserInput/Complete/Landmarks/Landmarks/LandmarkDetail.swift
@@ -12,26 +12,30 @@ struct LandmarkDetail: View {
     var landmark: Landmark
     
     var landmarkIndex: Int {
-        userData.landmarks.firstIndex(where: {$0.id == landmark.id})!
+        userData.landmarks.firstIndex(where: { $0.id == landmark.id })!
     }
-
+    
     var body: some View {
         VStack {
             MapView(coordinate: landmark.locationCoordinate)
+                .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
-
+            
             CircleImage(image: landmark.image)
                 .offset(x: 0, y: -130)
                 .padding(.bottom, -130)
-
+            
             VStack(alignment: .leading) {
                 HStack {
-                    Text(landmark.name)
+                    Text(verbatim: landmark.name)
                         .font(.title)
+                    
                     Button(action: {
-                        self.userData.landmarks[self.landmarkIndex].isFavorite.toggle()
+                        self.userData.landmarks[self.landmarkIndex]
+                            .isFavorite.toggle()
                     }) {
-                        if self.userData.landmarks[self.landmarkIndex].isFavorite {
+                        if self.userData.landmarks[self.landmarkIndex]
+                            .isFavorite {
                             Image(systemName: "star.fill")
                                 .foregroundColor(Color.yellow)
                         } else {
@@ -40,28 +44,28 @@ struct LandmarkDetail: View {
                         }
                     }
                 }
-
+                
                 HStack(alignment: .top) {
-                    Text(landmark.park)
+                    Text(verbatim: landmark.park)
                         .font(.subheadline)
                     Spacer()
-                    Text(landmark.state)
+                    Text(verbatim: landmark.state)
                         .font(.subheadline)
                 }
             }
             .padding()
-
+            
             Spacer()
         }
-        .navigationBarTitle(Text(verbatim: landmark.name), displayMode: .inline)
     }
 }
 
 #if DEBUG
-struct LandmarkDetail_Previews: PreviewProvider {
+struct LandmarkDetail_Preview: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail(landmark: landmarkData[0])
-        .environmentObject(UserData())
+        let userData = UserData()
+        return LandmarkDetail(landmark: userData.landmarks[0])
+            .environmentObject(userData)
     }
 }
 #endif
